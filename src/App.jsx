90f392753c517078c99b8401cc397d3e4811e6ad
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 19:43:03 +0300

    Revert "set proxy"
    This reverts commit 76d216e92e8223441fe009819a71ce2091131de3.

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,6 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Products from './components/Products';
 import NavBar from './components/NavBar';
-import Item from './components/Item';
 import Home from './components/Home';
 import './App.css';
 
@@ -12,7 +11,7 @@ function App() {
       <Routes>
         <Route path='/' element={<Home/>} />
         <Route path='products' element={<Products />} />
-        <Route path='products/:id' element={<Item />} />
+        <Route path='products/:id' element={<Product />} />
       </Routes>
     </Router>
   );
--- src/components/Products.jsx
+++ src/components/Products.jsx
@@ -1,48 +1,21 @@
-import { useNavigate } from "react-router-dom"
-import { useEffect, useState } from "react"
-import Loading from "./Loading"
-import Product from "./Product"
+import React from 'react'
 import "../styles/Products.css"
+import { useNavigate } from "react-router-dom"
 
 export default function Products() {
+  const navigate = useNavigate()
 
-    const navigate = useNavigate()
-    const [products, setProducts] = useState([])
-    const [loading, setLoading] = useState(true)
-
-    const fetchProducts = async () => {
-        fetch('/products')
-            .then((resp) => resp.json())
-            .then((data) => {
-                setLoading(false)
-                setProducts(data)
-            })
-            .catch((err) => console.log(err))
-    }
-
-    useEffect(() => {
-        fetchProducts()
-    }, [])
-
-    const ShowProducts = () => {
-        return (
-            <div>
-                <div className='headers'>
-                    <h1>FUNITURES</h1>
-                    <h3 onClick={() => { navigate('/addproduct') }}>ADD FUNITURE</h3>
-                </div>
-                <div className='products-wrapper'>
-                    {products.map((product, index) =>
-                        <Product product={product} key={index} />
-                    )}
-                </div>
-            </div>
-        )
-    }
-    
-    return (
-        <div className='products'>
-            {loading ? <Loading /> : <ShowProducts/>}
+  return (
+    <div className='products'>
+        <div className='headers'>
+          <h1>FUNITURES</h1>
+          <h3 onClick={() => {navigate('/addproduct')}}>ADD FUNITURE</h3>
         </div>
-    )
+        <div className='products-wrapper'>
+          {/* {products.map((product, index) =>
+              <Product product={product} key={index}/>
+          )}  */}
+        </div>
+    </div>
+  )
 }
