b0a897a56a1094bf97ade3d04956145b734da7f7
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 23:18:29 +0300

    add contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,18 @@
+import ContactForm from './ContactForm'
+import "../styles/Contact.css"
+
+export default function Contact() {
+  return (
+    <div className='contact-us'>
+      <div className='contact-message'>
+        <div className='message'>
+          <h1>Contact Us</h1>
+          <p>Need to get in touch with us? Either fill in the inquiry form or find the department email you'd like to contact below.</p>
+        </div>
+      </div>
+      <div className='contact-form'>
+        <ContactForm />
+      </div>
+    </div>
+  )
+}
