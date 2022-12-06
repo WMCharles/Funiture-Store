3ec3bc07f9f8c5ca16d77f3ae4f9aada7c1a3d67
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 21:59:33 +0300

    add Cart components

---
+++ src/components/Cart.jsx
@@ -0,0 +1,46 @@
+import React, { useEffect, useState } from 'react'
+
+import "./Cart.css"
+import CartItem from './CartItem'
+
+export default function Cart({ cart, removeItem, handleChange }) {
+  const [price, setPrice] = useState(0)
+
+  function handlePrice() {
+    let total = 0
+    // eslint-disable-next-line no-lone-blocks
+    { cart.map((item) => (total += item.price * item.quantity)) }
+    setPrice(total)
+  }
+
+  useEffect(() => {
+    handlePrice()
+  })
+  return (
+    <div>
+      {cart.length === 0 && 
+        <div className="cart">
+          <p>Your cart is empty</p>
+        </div>
+      }
+      {cart.length > 0 &&
+        <div className='cart'>
+          <h1>Shopping Cart</h1>
+          {cart.map((item, index) =>
+            <CartItem item={item} removeItem={removeItem} key={index} handleChange={handleChange} />
+          )}
+          <hr />
+          <div className='net'>
+            <div></div>
+            <div></div>
+            <div></div>
+            <div className='checkout'>
+              <h1>$ {price}</h1>
+              <button>Checkout</button>
+            </div>
+          </div>
+        </div>
+      }
+    </div>
+  )
+}
---
+++ src/components/CartItem.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+export default function CartItem({item, removeItem, handleChange}) {
+    function removeItemFromCart(){
+        removeItem(item)
+    }
+    return (
+        <div className='item'>
+            <div className='image'>
+                <img src={item.image_url} alt="" />
+            </div>
+            <div className='description'>
+                <div>
+                <h3>{item.title}</h3>
+                <br/>
+                <h3 className='price'>$ {item.price}</h3>
+                </div>
+            </div>
+            <div className='quantity'>
+                <button onClick={() => handleChange(item, 1)}>+</button> {item.quantity} <button onClick={() => handleChange(item, -1)}>-</button>
+            </div>
+            <div className='total-amount'>
+                <h2>$ {item.price * item.quantity}</h2>
+                <br/>
+                <p onClick={() => removeItemFromCart(item)}>Remove</p>
+            </div>
+        </div>
+    )
+}
