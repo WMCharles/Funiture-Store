47f60d1e37ecff2d37b66cfb4909bc79a9494b37
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 19:10:25 +0300

    add styles folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,19 @@
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
+import Products from './components/Products';
+import NavBar from './components/NavBar';
+import Home from './components/Home';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      
-    </div>
+    <Router>
+      <NavBar />
+      <Routes>
+        <Route path='/' element={<Home/>} />
+        <Route path='products' element={<Products />} />
+        <Route path='products/:id' element={<Product />} />
+      </Routes>
+    </Router>
   );
 }
 
