529603a1ec8ffb430e1d45f2f94236f5c5acf58f
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-06 18:52:59 +0300

    add NavBar component && its styles

---
+++ src/components/NavBar.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import { Link, Outlet } from "react-router-dom"
+
+export default function NavBar() {
+  return (
+    <div className='header'>
+      <div className='navbar-container'>
+        <Link className="navbar-logo" to="/">FUNiTURES</Link> <div className='nav-menu'>
+          <Link className='nav-links' to="/">Home</Link>
+          <Link className='nav-links' to="/products">Products</Link>
+          <Link className='nav-links' to="/cart">Cart</Link>
+          <Link className='nav-links' to="/contact">Contact</Link>
+        </div>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
