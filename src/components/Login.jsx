db788f55f6f8873dddaaa8975f3f7ea0a1bfc4c3
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-07 17:08:09 +0300

    add Error handling

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,60 @@
-export default function Login({handleClick}) {
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+export default function Login({ handleClick }) {
+
+    const [username, setUsername] = useState("");
+    const [password, setPassword] = useState("");
+    const [errors, setErrors] = useState([]);
+    const navigate = useNavigate()
+    // eslint-disable-next-line no-unused-vars
+    const [isLoading, setIsLoading] = useState(false);
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setIsLoading(true);
+        fetch("/login", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({ username, password }),
+        }).then((r) => {
+            setIsLoading(false);
+            if (r.ok) {
+                r.json().then((user) => console.log(user));
+                navigate(`/products`)
+            } else {
+                r.json().then((err) => setErrors(err.errors));
+            }
+        });
+    }
+
     return (
         <div>
-            <form id="form">
+            <form id="form" onSubmit={handleSubmit}>
                 <h1>Login</h1>
-                <div class="input-control">
-                    <label for="username">Username</label>
-                    <input type="text" id="username" name="username" />
+                <div className="input-control">
+                    <label htmlFor="username">Username</label>
+                    <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
-               
-                <div class="input-control">
-                    <label for="password">Password</label>
-                    <input type="password" id="password" name="password" />
-                </div>     
+
+                <div className="input-control">
+                    <label htmlFor="password">Password</label>
+                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+                </div>
+                {errors.length > 0 &&
+                    <div className='input-control'>
+                        <div style={{ color: "red" }}>
+                            {errors.map((error, index) =>
+                                <p key={index}>{error}</p>
+                            )}
+                        </div>
+                    </div>
+                }
                 <div className="input-control">
                     <button type="submit">Login</button>
-                </div>           
+                </div>
                 <div className="input-control">
                     <p className='p'>Don't have an account ? <span onClick={handleClick}>Sign Up</span> </p>
                 </div>
