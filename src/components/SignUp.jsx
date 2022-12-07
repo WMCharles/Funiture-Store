91a2c5ab0954a3a2b4d034bc8abfb42020881c3b
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-07 17:24:00 +0300

    update Signup functionality

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,25 +1,71 @@
+import { useState } from 'react';
+import { useNavigate } from 'react-router-dom';
 import '../styles/SignUp.css'
 
-export default function SignUp({handleClick}) {
+export default function SignUp({ handleClick }) {
+
+    const [username, setUsername] = useState("");
+    const [email, setEmail] = useState("");
+    const [password, setPassword] = useState("");
+    const [passwordConfirmation, setPasswordConfirmation] = useState("");
+    const [errors, setErrors] = useState([]);
+    // eslint-disable-next-line no-unused-vars
+    const [isLoading, setIsLoading] = useState(false);
+    const navigate = useNavigate()
+
+    function handleSubmit(e) {
+        e.preventDefault();
+        setErrors([]);
+        setIsLoading(true);
+        fetch("/signup", {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify({
+                username,
+                email,
+                password,
+                password_confirmation: passwordConfirmation,
+            }),
+        }).then((r) => {
+            setIsLoading(false);
+            if (r.ok) {
+                r.json().then((user) => console.log(user));
+                navigate(`/products`)
+            } else {
+                r.json().then((err) => setErrors(err.errors));
+            }
+        });
+    }
     return (
         <div>
-            <form id="form">
+            <form id="form" onSubmit={handleSubmit}>
                 <h1>Registration</h1>
                 <div class="input-control">
                     <label for="username">Username</label>
-                    <input type="text" id="username" name="username" />
+                    <input type="text" id="username" name="username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 </div>
                 <div class="input-control">
                     <label for="email">Email</label>
-                    <input type="text" id="email" name="email" />
+                    <input type="email" id="email" name="email" value={email} onChange={(e) => setEmail(e.target.value)} />
                 </div>
                 <div class="input-control">
                     <label for="password">Password</label>
-                    <input type="password" id="password" name="password" />
+                    <input type="password" id="password" name="password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 </div>
                 <div class="input-control">
                     <label for="password">Password</label>
-                    <input type="password2" id="password2" name="password2" />
+                    <input type="password" id="password2" name="password_confirmation" value={passwordConfirmation} onChange={(e) => setPasswordConfirmation(e.target.value)} />
+                </div>
+                <div className='input-group'>
+                    {errors.length > 0 && (
+                        <div style={{ color: "red" }}>
+                            {errors.map((error) => (
+                                <p key={error}>{error}</p>
+                            ))}
+                        </div>
+                    )}
                 </div>
                 <div className="input-control">
                     <button type="submit">Register</button>
