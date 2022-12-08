dd5f4eb619c3ddeb61f26431d191290c46ec7d16
WMCharles <charles.wafula@student.moringaschool.com>
2022-12-08 17:31:58 +0300

    add props

--- src/components/Filter.jsx
+++ src/components/Filter.jsx
@@ -1,25 +1,20 @@
 import "../styles/Filter.css";
 
-export default function Filter({ input, setFilter, filterProducts, products }) {
+export default function Filter({input, setFilter, filterProducts, products }) {
   return (
     <div className="filter">
       <div className="category-buttons">
         <p className="instruction">Categories:</p>
         <p onClick={() => setFilter(products)}>All</p>
         <p onClick={() => filterProducts("Living-Room")}>Living Room</p>
-        <p onClick={() => filterProducts("Dining-Room")}>Dining Room</p>
+        <p onClick={() => filterProducts("Dining")}>Dining Room</p>
         <p onClick={() => filterProducts("Kitchen")}>Kitchen</p>
         <p onClick={() => filterProducts("Bedroom")}>Bedroom</p>
       </div>
       <div className="filter-container">
         <div className="wrap">
           <div className="search">
-            <input
-              type="text"
-              className="searchTerm"
-              placeholder="What product are you looking for?"
-              onChange={(e) => input(e.target.value.toLocaleUpperCase())}
-            />
+            <input type="text" className="searchTerm" placeholder="What product are you looking for?" onChange={(e) => input(e.target.value.toLocaleUpperCase())}/>
             <button type="submit" className="searchButton">
               <i className="fa fa-search"></i>
             </button>
